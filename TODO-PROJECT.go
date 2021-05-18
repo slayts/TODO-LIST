93f3d3d9e831bbe3c435005bf41ec0fead93ac9f
Yazan <contact.yazan@gmail.com>
2021-05-18 19:01:10 +0300

    basic scan from user, to be updated to flags

--- TODO-PROJECT.go
+++ TODO-PROJECT.go
@@ -9,35 +9,38 @@ type element struct {
 
 //view elements in our list
 func viewlist(l map[int]string) {
-	fmt.Println("to do ", l)
+	fmt.Println("\nto do ", l)
 }
 
 //add element to our list
 func addtodo() {
-
+	fmt.Println("\nthis is add")
 }
 
 //update element in our list
 func updatetodo() {
-
+	fmt.Println("\nthis is update")
 }
 
 //delete from list (takes id and delete from map by id)
 func deletetodo() {
-
+	fmt.Println("\nthis is delete")
 }
 
 //marks if done returns boolean
 func markasdone() {
-
+	fmt.Println("\nthis is marking as done / not done")
 }
 
 //filters results based on date (to be done or when updated/added?)
 func datefilter() {
-
+	fmt.Println("\nthis is finding by date")
 }
 
 func main() {
+	var s int
+	fmt.Println("\nSellect from the following:-\n\n1.View List\n\n2.Add an element\n\n3.Update an element\n\n4.Delete an element\n\n5.Mark if done\n\n6.Find task by date")
+	fmt.Scanln(&s)
 
 	todo := element{
 		id:  0,
@@ -47,11 +50,22 @@ func main() {
 		todo.id: todo.txt,
 	}
 
+	switch s {
+	case 1:
+		viewlist(list)
+	case 2:
+		addtodo()
+	case 3:
+		updatetodo()
+	case 4:
+		deletetodo()
+	case 5:
+		markasdone()
+	case 6:
+		datefilter()
+
+	}
+
 	//send list to all
-	viewlist(list)
-	addtodo()
-	updatetodo()
-	deletetodo()
-	markasdone()
-	datefilter()
+
 }
