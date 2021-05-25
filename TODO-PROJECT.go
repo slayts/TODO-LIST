cdd2655d6acec35317b1834c47e0b76a66c97e33
Yazan <contact.yazan@gmail.com>
2021-05-26 02:26:00 +0300

    KSUID

--- TODO-PROJECT.go
+++ TODO-PROJECT.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/segmentio/ksuid"
 )
 
 type element struct {
-	id  int
-	txt string
+	id          ksuid.KSUID
+	title       string
+	description string
+	duedate     time.Time
 }
 
 //view elements in our list
@@ -15,8 +20,24 @@ func viewlist(l map[int]string) {
 }
 
 //add element to our list
-func addtodo(l map[int]string) {
-	fmt.Println("\nthis is add")
+func addtodo(e element, l map[int]string) {
+	//assigning a UUID
+	id := ksuid.New()
+	e.id = id
+	//inputing the title
+	title := e.title
+	fmt.Println("what are you going TO-DO?")
+	fmt.Scanln(&title)
+	//inputing description
+	description := e.description
+	fmt.Println("describe what do you want to do exaclt?")
+	fmt.Scanln(&description)
+	//inputing due date for the element
+	//fmt.Println("when is tis task due (year, month, day) ?")
+
+	fmt.Println(title)
+	fmt.Println(description)
+
 }
 
 //update element in our list
@@ -45,20 +66,15 @@ func main() {
 	fmt.Println("\nSellect from the following:-\n\n1.View List\n\n2.Add an element\n\n3.Update an element\n\n4.Delete an element\n\n5.Mark if done\n\n6.Find task by date")
 	fmt.Scanln(&s)
 	//initializing element
-	todo := element{
-		id:  0,
-		txt: "",
-	}
+	todo := element{}
 	//this is the TODO list
-	list := map[int]string{
-		todo.id: todo.txt,
-	}
+	list := map[int]string{}
 
 	switch s {
 	case 1:
 		viewlist(list)
 	case 2:
-		addtodo(list)
+		addtodo(todo, list)
 	case 3:
 		updatetodo(list)
 	case 4:
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module yaz.com
 
 go 1.16
 
-require google.golang.org/grpc v1.38.0 // indirect
+require (
+	github.com/segmentio/ksuid v1.0.3 // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+)
