7c83dd3b24f28b65ba2a1f5f395551b0a765bb54
Yazan <contact.yazan@gmail.com>
2021-05-26 16:01:18 +0300

    ksuid as a key that returns an element struct

--- TODO-PROJECT.go
+++ TODO-PROJECT.go
@@ -15,12 +15,12 @@ type element struct {
 }
 
 //view elements in our list
-func viewlist(l map[int]string) {
+func viewlist(l map[ksuid.KSUID]element) {
 	fmt.Println("\nto do\n", l)
 }
 
 //add element to our list
-func addtodo(e element, l map[int]string) {
+func addtodo(e element, l map[ksuid.KSUID]element) {
 	//assigning a UUID
 	id := ksuid.New()
 	e.id = id
@@ -35,28 +35,25 @@ func addtodo(e element, l map[int]string) {
 	//inputing due date for the element
 	//fmt.Println("when is tis task due (year, month, day) ?")
 
-	fmt.Println(title)
-	fmt.Println(description)
-
 }
 
 //update element in our list
-func updatetodo(l map[int]string) {
+func updatetodo(l map[ksuid.KSUID]element) {
 	fmt.Println("\nthis is update")
 }
 
 //delete from list (takes id and delete from map by id)
-func deletetodo(l map[int]string) {
+func deletetodo(l map[ksuid.KSUID]element) {
 	fmt.Println("\nthis is delete")
 }
 
 //marks if done returns boolean
-func markasdone(l map[int]string) {
+func markasdone(l map[ksuid.KSUID]element) {
 	fmt.Println("\nthis is marking as done / not done")
 }
 
 //filters results based on date (to be done or when updated/added?)
-func datefilter(l map[int]string) {
+func datefilter(l map[ksuid.KSUID]element) {
 	fmt.Println("\nthis is finding by date")
 }
 
@@ -68,7 +65,7 @@ func main() {
 	//initializing element
 	todo := element{}
 	//this is the TODO list
-	list := map[int]string{}
+	list := map[ksuid.KSUID]element{}
 
 	switch s {
 	case 1:
